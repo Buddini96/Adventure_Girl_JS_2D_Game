e5796bd2432416256c6e49932c243f967072c814
buddini96 <buddinikl1996@gmail.com>
2023-10-03 14:19:23 +0530

    Run Animation Added

--- assests/js/Jscript.js
+++ assests/js/Jscript.js
@@ -2,7 +2,11 @@
 
   idleImageNumber = 0;
   idleAnimationNumber = 0;
+  runImageNumber = 0;
+  runAnimationNumber = 0;
 
+
+    //Idle animation
     function idleAnimation(){
         idleImageNumber  = idleImageNumber + 1;
         if (idleImageNumber == 11){
@@ -14,4 +18,47 @@
     function idleAnimationStart(){
         idleAnimationNumber = setInterval(idleAnimation,200);
     }
+
+
+    //Run Animation
+
+    function runAnimation(){
+        runImageNumber  = runImageNumber + 1;
+        if (runImageNumber == 9){
+            runImageNumber = 1;
+        }
+        girl.src = "assests/imges/run ("+runImageNumber+").png";
+    }
+
+    function runAnimationStart(){
+        runAnimationNumber = setInterval(runAnimation,100);
+        clearInterval(idleAnimationNumber);
+    }
+
+    function keyCheck(event){
+        //alert(event.which);
+        //enter button key 13
+
+        var keyCode = event.which;
+
+        if (keyCode == 13){
+            if (runAnimationNumber == 0){
+                runAnimationStart();
+            }
+        }
+
+        if (moveBackgroundAnimationId == 0){
+            moveBackgroundAnimationId = setInterval(moveBackground,100);
+        }
+    }
+
+    var backgroundImagePositionX = 0;
+    var moveBackgroundAnimationId = 0;
+    function moveBackground(){
+        backgroundImagePositionX = backgroundImagePositionX - 20;
+        document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px";
+    }
+
+
+
 
