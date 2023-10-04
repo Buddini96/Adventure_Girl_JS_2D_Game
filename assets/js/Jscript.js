9dd45451d47e0dd2bd46dde85e88f9683c885744
buddini96 <buddinikl1996@gmail.com>
2023-10-04 20:14:27 +0530

    Slide Animation Added

--- assets/js/Jscript.js
+++ assets/js/Jscript.js
@@ -5,7 +5,9 @@
   runImageNumber = 1;
   runAnimationNumber = 0;
   jumpAnimationNumber = 0;
-    jumpImageNumber = 1;
+  jumpImageNumber = 1;
+  slideAnimationNumber = 0;
+  slideImageNumber = 1;
 
 
     //Idle animation
@@ -23,7 +25,6 @@
 
 
     //Run Animation
-
     function runAnimation(){
         runImageNumber  = runImageNumber + 1;
         if (runImageNumber == 9){
@@ -61,6 +62,36 @@
         girl.src = "./assets/images/Jump("+jumpImageNumber+").png";
     }
 
+     //Slide Animation
+     function slideAnimation(){
+         slideImageNumber  = slideImageNumber + 1;
+
+         // if (slideImageNumber <= 2){
+         //     girlMarginTop = girlMarginTop + 5;
+         //     girl.style.marginTop = girlMarginTop + "px";
+         // }
+         //
+         // if (slideImageNumber >= 5){
+         //     girlMarginTop = girlMarginTop + 5;
+         //     girl.style.marginTop = girlMarginTop + "px";
+         // }
+         if (slideImageNumber == 4){
+             slideImageNumber = 1;
+             clearInterval(slideAnimationNumber);
+             slideAnimationNumber = 0;
+             slideImageNumber = 0;
+             runAnimationStart();
+         }
+         girl.src = "./assets/images/Slide("+slideImageNumber+").png";
+     }
+
+     function slideAnimationStart(){
+         clearInterval(runAnimationNumber);
+         runImageNumber=0;
+         clearInterval(runAnimationNumber);
+         slideAnimationNumber = setInterval(slideAnimation,100);
+     }
+
     function jumpAnimationStart(){
         clearInterval(idleAnimationNumber);
         runImageNumber=0;
@@ -72,6 +103,7 @@
         // alert(event.which);
         //enter button key code 13
         //space button key code 32
+        //z button key code 122 - slide
 
         var keyCode = event.which;
 
@@ -91,6 +123,18 @@
         if (keyCode == 32){
             if(jumpAnimationNumber == 0){
                 jumpAnimationStart();
+            }
+            if (moveBackgroundAnimationId == 0){
+                moveBackgroundAnimationId = setInterval(moveBackground,100);
+            }
+            if (boxAnimationId == 0){
+                boxAnimationId = setInterval(boxAnimation,100);
+            }
+        }
+
+        if (keyCode == 122){
+            if(slideAnimationNumber == 0){
+                slideAnimationStart();
             }
             if (moveBackgroundAnimationId == 0){
                 moveBackgroundAnimationId = setInterval(moveBackground,100);
